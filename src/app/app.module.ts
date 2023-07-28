a5442f23bfe77d72fd15330fc07309b83f530986
inablackwood <inablackwood@gmail.com>
2023-07-28 17:46:09 +0200

    Added definitive favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    PresentationComponent,
+    MainMenuComponent,
+
   ],
   imports: [
     BrowserModule
